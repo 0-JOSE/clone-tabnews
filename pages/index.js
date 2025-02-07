c76ff47462f814b85cb16c20a3f5342d61d15502
José Adenilson <eu.jose@outlook.com.br>
2025-02-07 02:33:20 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>E quando estivermos bem velhinhos, vou segurar sua mão e dizer: Viu deu tudo certo.</h1>
+    return <h1>E quando estivermos bem velhinhos, vou segurar sua mão e dizer: Viu deu tudo certo...</h1>
 }
 export default Home;
