193fbabfec5ab420fd49db5f5f0ea6a8a641d146
José Adenilson <eu.jose@outlook.com.br>
2025-02-07 02:40:52 +0000

    adiciona emoji

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>E quando estivermos bem velhinhos, vou segurar sua mão e dizer: Viu deu tudo certo...</h1>
+    return <h1>E quando estivermos bem velhinhos, vou segurar sua mão e dizer: Viu deu tudo certo 🥰</h1>
 }
 export default Home;
